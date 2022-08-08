006d6fca9fb1f079dc4540cd2c46888c3ee10a65
javisof <110834109+javisof@users.noreply.github.com>
2022-08-08 19:29:21 +0200

    Delete ListView directory

--- ListView/mostrar_datos/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Book, Category
-
-# Register models in Admin Panel
-admin.site.register(Book)
-admin.site.register(Category)
--- ListView/mostrar_datos/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MostrarDatosConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mostrar_datos'
